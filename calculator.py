de7576e03bb7917cb569eceb641fdf8192862289
Aryan Jha <aryanjha@gmail.com>
2020-04-01 15:43:14 -0400

    clears box after displaying answer

--- calculator.py
+++ calculator.py
@@ -19,11 +19,17 @@ def isnum(first_num):
 
 
 def button_click(number):
+    global equal_check
     if(isnum(e.get())):
-        curr = e.get()
-        e.delete(0, END)
-        # make sure these are strings so they don't add.
-        e.insert(0, str(curr) + str(number))
+        if equal_check:
+            e.delete(0, END)
+            e.insert(0, str(number))
+            equal_check = False
+        else:
+            curr = e.get()
+            e.delete(0, END)
+            # make sure these are strings so they don't add.
+            e.insert(0, str(curr) + str(number))
     else:
         e.delete(0, END)
         e.insert(0, str(number))
@@ -66,6 +72,7 @@ def getnum():
 
 
 def equals():
+    global equal_check
     second_num = float(e.get())
     if operation == "+":
         e.delete(0, END)
@@ -82,6 +89,8 @@ def equals():
     elif operation == "/" and second_num == 0:
         e.delete(0, END)
         e.insert(0, str("Error, divide by 0"))
+    equal_check = True  # we have displayed an answer to the screen, so the next time we input a number,
+    # we must clear the answer
 
 
 # Defining buttons
@@ -98,33 +107,33 @@ button_9 = Button(root, text="9", padx=40, pady=20, width=3, command=lambda: but
 button_0 = Button(root, text="0", padx=40, pady=20, width=3, command=lambda: button_click(0))
 
 # only need lambda if you are going to pass something into the button command.
-button_add = Button(root, text="+", padx=39, pady=20, width=3, command=add)
-button_subtract = Button(root, text="-", padx=39, pady=20, width=3, command=subtract)
-button_multiply = Button(root, text="X", padx=39, pady=20, width=3, command=multiply)
-button_divide = Button(root, text="÷", padx=39, pady=20, width=3, command=divide)
-button_clear = Button(root, text="Clear", padx=79, pady=20, width=7, command=button_clear)
-button_equal = Button(root, text="=", padx=79, pady=20, width=7, command=equals)
+button_add = Button(root, text="+", padx=40, pady=20, width=3, command=add)
+button_subtract = Button(root, text="-", padx=40, pady=20, width=3, command=subtract)
+button_multiply = Button(root, text="x", padx=40, pady=20, width=3, command=multiply)
+button_divide = Button(root, text="÷", padx=40, pady=20, width=3, command=divide)
+button_clear = Button(root, text="Clear", padx=40, pady=20, width=3, command=button_clear)
+button_equal = Button(root, text="=", padx=40, pady=20, width=3, command=equals)
 
 # Put buttons on the screen
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
+button_7.grid(row=2, column=0)
+button_8.grid(row=2, column=1)
+button_9.grid(row=2, column=2)
 
-button_0.grid(row=4, column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
-button_add.grid(row=5, column=0)
-button_subtract.grid(row=6, column=0)
-button_multiply.grid(row=6, column=1)
-button_divide.grid(row=6, column=2)
-button_equal.grid(row=5, column=1, columnspan=2)
+button_0.grid(row=5, column=0)
+button_clear.grid(row=1, column=3)
+button_add.grid(row=5, column=3)
+button_subtract.grid(row=4, column=3)
+button_multiply.grid(row=3, column=3)
+button_divide.grid(row=2, column=3)
+button_equal.grid(row=5, column=2)
 
 root.mainloop()
